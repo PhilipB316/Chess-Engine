3dbdaaee710a4c6c8af5bf48a19fc8a49ae35ad5
Philip Brand <>
2024-11-28 17:13:49 +1300

    tidied rook magic numbers, included hardcoded ones

--- bit-board chess engine/magic_numbers.c
+++ bit-board chess engine/magic_numbers.c
@@ -27,30 +27,20 @@
 #include "lookuptables.h"
 #include "movefinder.h"
 
+#define KNOWN_MAGIC_NUMBERS 1
+
 // masks to determine relevant blockers for rooks and bishops
 static ULL rook_blocker_masks[64];
 static ULL bishop_blocker_masks[64];
 
 // magic numbers for rooks and bishops
-static ULL rook_magic_numbers[64];
+static ULL array_for_rook_magic_numbers[64];
 static ULL bishop_magic_numbers[64];
 
 // lookup tables for rook and bishop attacks
 static ULL rook_attack_lookup_table[64][4096];
 static ULL bishop_attack_lookup_table[64][512];
 
-
-// number of possible bits rook blockers can occupy
-uint8_t RBits[64] = {
-  12, 11, 11, 11, 11, 11, 11, 12,
-  11, 10, 10, 10, 10, 10, 10, 11,
-  11, 10, 10, 10, 10, 10, 10, 11,
-  11, 10, 10, 10, 10, 10, 10, 11,
-  11, 10, 10, 10, 10, 10, 10, 11,
-  11, 10, 10, 10, 10, 10, 10, 11,
-  11, 10, 10, 10, 10, 10, 10, 11,
-  12, 11, 11, 11, 11, 11, 11, 12
-};
 
 // (64 - bits) numbers of bits rook blockers can occupy 
 uint8_t offset_RBits[64] = 
@@ -64,6 +54,75 @@ uint8_t offset_RBits[64] =
     53, 54, 54, 54, 54, 54, 54, 53,
     52, 53, 53, 53, 53, 53, 53, 52
 };
+
+const static ULL actual_rook_magic_numbers[64] = 
+{
+756607761056301088ULL,
+4917965982829391872ULL,
+9259436018514335872ULL,
+252227969560612864ULL,
+1873504042192277572ULL,
+4683744712145502216ULL,
+10736588108788171008ULL,
+72060078754236160ULL,
+140880296050688ULL,
+36239972015016000ULL,
+288793611754082336ULL,
+10980338876383625296ULL,
+281562084086016ULL,
+7219973898715005952ULL,
+288511855423520780ULL,
+703743277449344ULL,
+10430337286754283586ULL,
+4616189892934762561ULL,
+845524712292416ULL,
+738733825827278848ULL,
+81910317868647472ULL,
+10133648984637568ULL,
+27025995828578306ULL,
+24807181388087505ULL,
+4611826895502458880ULL,
+290517918687560456ULL,
+436853014294175744ULL,
+3891391591680573472ULL,
+8798240768384ULL,
+54608346652868736ULL,
+36037885170811138ULL,
+4621327094725559297ULL,
+18020034588704898ULL,
+794920529348804608ULL,
+38984971611803648ULL,
+4611765185420464128ULL,
+653444742557733889ULL,
+9379309380144470164ULL,
+4616304006254301322ULL,
+5089704017996ULL,
+2305983773545627684ULL,
+1478940450466431008ULL,
+282162185437249ULL,
+36310615627333664ULL,
+145241122350858244ULL,
+563259460157452ULL,
+77124178800672772ULL,
+2533275872591874ULL,
+306325047602086400ULL,
+9876464420289249344ULL,
+7206920489413936256ULL,
+5440992801384890624ULL,
+290490978518433920ULL,
+4400196747392ULL,
+9377629124748796928ULL,
+4611734676179550720ULL,
+72093878194930306ULL,
+1299358895885811745ULL,
+3603162595299696641ULL,
+1424967491126529ULL,
+72339077738795013ULL,
+563518231807746ULL,
+4505833060814860ULL,
+4684308548237328530ULL
+};
+
 
 
 /**
@@ -183,7 +242,7 @@ ULL determine_possible_rook_moves(uint8_t sq, ULL blocker)
 
 
 /**
- * @brief Generates all possible rook blockers and their corresponding magic numbers.
+ * @brief Generates all possible blockers and their corresponding magic numbers.
  * 
  * FOR EVERY SQUARE ON THE _BOARD (although the function is called individually for each square):
  * 
@@ -205,17 +264,23 @@ ULL determine_possible_rook_moves(uint8_t sq, ULL blocker)
  * Once the function returns true, the magic number is stored in the magic number array,
  * and a new square is passed into the function.
  * 
- * @param square The square index of the rook.
+ * @param square The square index of the piece.
  * @return true if the magic number was generated successfully, false otherwise.
-
  */
 bool generate_possible_rook_blockers_and_magic_numbers(uint8_t square)
 {
     ULL mask = rook_blocker_masks[square];
     int bits = __builtin_popcountll(mask);
     int permutations = 1 << bits;
-    ULL magic_number = random_ULL_fewbits();
-    // static ULL counter = 0;
+    ULL magic_number;
+
+    if (KNOWN_MAGIC_NUMBERS) 
+    {
+        magic_number = actual_rook_magic_numbers[square];
+    } else 
+    {
+        magic_number = random_ULL_fewbits();
+    }
 
     // all the permutations are simply numbers from 0 to 2^bits
     // this code sets the blocker mask bits to that permutation value
@@ -237,20 +302,35 @@ bool generate_possible_rook_blockers_and_magic_numbers(uint8_t square)
                 bit_index++;
             }
         }
+
         // determine possible moves for that permutation
         ULL possible_moves = determine_possible_rook_moves(square, blocker);
         ULL index = (blocker * magic_number) >> (64 - bits);
         ULL current = rook_attack_lookup_table[square][index];
-        // if there is no collision, store the possible moves in the lookup table
-        if (!current || current == possible_moves) {
+
+
+        if (KNOWN_MAGIC_NUMBERS) 
+        {
             rook_attack_lookup_table[square][index] = possible_moves;
-        } else {
-            // counter++;
-            return 0;
+            array_for_rook_magic_numbers[square] = magic_number;
+        } else 
+        {
+            // if there is no collision, store the possible moves in the lookup table
+            if (current == possible_moves) {
+                continue;
+            } else if (!current) {
+                rook_attack_lookup_table[square][index] = possible_moves;
+            } else {
+                // counter++;
+                return 0;
+            }
         }
     }
-    // printf("Collision detected %llu times\n", counter);
-    rook_magic_numbers[square] = magic_number;
+
+    if (!KNOWN_MAGIC_NUMBERS) {
+        // printf("Collision detected %llu times\n", counter);
+        array_for_rook_magic_numbers[square] = magic_number;
+    }
     return 1;
 }
 
@@ -260,7 +340,6 @@ bool generate_possible_rook_blockers_and_magic_numbers(uint8_t square)
 int main(void)
 {
     generate_rook_blocker_masks();
-    printf("Generated masks\n");
     for (int j = 0; j < 64; j++)
     {
         printf("Square %d\n", j);
@@ -273,6 +352,7 @@ int main(void)
         }
     }
 
+
     for (int i = 0; i < 64; i++)
     {   
         printf("Square: %d\n", i);
@@ -281,8 +361,7 @@ int main(void)
         ULL blockers = rook_blocker_masks[i] & random_position;
         print_bitboard(blockers);
 
-        uint8_t bits = RBits[i];
-        ULL index = (blockers * rook_magic_numbers[i]) >> (64 - bits);
+        ULL index = (blockers * array_for_rook_magic_numbers[i]) >> offset_RBits[i];
         printf("Moves: \n");
         print_bitboard(rook_attack_lookup_table[i][index]);
         srand(index);
