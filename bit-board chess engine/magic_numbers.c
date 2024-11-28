dd739b306326a00e205181b2a222bfe8f6600b42
Philip Brand <>
2024-11-28 18:25:06 +1300

    add code to generate bishop magic numbers
    - bishop magic numbers are now also hard-coded

--- bit-board chess engine/magic_numbers.c
+++ bit-board chess engine/magic_numbers.c
@@ -35,11 +35,11 @@ static ULL bishop_blocker_masks[64];
 
 // magic numbers for rooks and bishops
 static ULL array_for_rook_magic_numbers[64];
-static ULL bishop_magic_numbers[64];
+static ULL array_for_bishop_random_numbers[64];
 
 // lookup tables for rook and bishop attacks
 static ULL rook_attack_lookup_table[64][4096];
-static ULL bishop_attack_lookup_table[64][512];
+static ULL bishop_attack_lookup_table[64][4096];
 
 
 // (64 - bits) numbers of bits rook blockers can occupy 
@@ -55,7 +55,19 @@ uint8_t offset_RBits[64] =
     52, 53, 53, 53, 53, 53, 53, 52
 };
 
-const static ULL actual_rook_magic_numbers[64] = 
+uint8_t offset_BBits[64] = {
+    58, 59, 59, 59, 59, 59, 59, 58,
+    59, 59, 59, 59, 59, 59, 59, 59,
+    59, 59, 57, 57, 57, 57, 59, 59,
+    59, 59, 57, 55, 55, 57, 59, 59,
+    59, 59, 57, 55, 55, 57, 59, 59,
+    59, 59, 57, 57, 57, 57, 59, 59,
+    59, 59, 59, 59, 59, 59, 59, 59,
+    58, 59, 59, 59, 59, 59, 59, 58
+};
+
+
+static ULL actual_rook_magic_numbers[64] = 
 {
 756607761056301088ULL,
 4917965982829391872ULL,
@@ -123,7 +135,73 @@ const static ULL actual_rook_magic_numbers[64] =
 4684308548237328530ULL
 };
 
-
+static ULL actual_bishop_magic_numbers[64] = 
+{
+306249795545277056ULL,
+1162212637740662848ULL,
+289360676286103624ULL,
+5243390635171676304ULL,
+2882603104200097792ULL,
+4683893164334123008ULL,
+108369034312966208ULL,
+10448632889718345216ULL,
+1315196812053775361ULL,
+2305865171916357668ULL,
+17609404534784ULL,
+72625234104025600ULL,
+9297699607545978880ULL,
+12141846991017607238ULL,
+4613938402373935104ULL,
+27171286031832064ULL,
+73254000197371920ULL,
+2814889423077888ULL,
+4515730770891008ULL,
+1134704608805121ULL,
+19140850330107904ULL,
+35330409635840ULL,
+5766296684326815752ULL,
+35185530243074ULL,
+37735239604241921ULL,
+12106247587906006018ULL,
+36100342987360512ULL,
+563639429922976ULL,
+11097152056330305536ULL,
+11259548875620480ULL,
+148902530494497815ULL,
+288512132585228294ULL,
+36680056869494790ULL,
+5642702541828096ULL,
+39754220437632ULL,
+54641884442132992ULL,
+2607584742593274368ULL,
+1157442701017251904ULL,
+9313731006232135746ULL,
+143076114908165ULL,
+9226754814304585732ULL,
+5333392267665213472ULL,
+9576754884907520ULL,
+243352993028900865ULL,
+612947504647898112ULL,
+27056971119068192ULL,
+1298237361628779520ULL,
+2278464086360128ULL,
+28151908957946880ULL,
+864973187564767744ULL,
+578713668826696704ULL,
+288309750402252864ULL,
+36029140650033236ULL,
+4611699299675931792ULL,
+4591564869607496ULL,
+1157442705014538240ULL,
+109989843701824ULL,
+11003723073600ULL,
+37155258123751426ULL,
+72075753168315402ULL,
+297307978783131144ULL,
+10178173658079437056ULL,
+5332297169252787209ULL,
+722917911187031552ULL
+};
 
 /**
  * @brief Generates a random unsigned long long integer.
@@ -195,6 +273,38 @@ void generate_rook_blocker_masks(void)
     }
 }
 
+void generate_bishop_blocker_masks(void)
+{
+    for (int square = 0; square < 64; square++)
+    {
+        ULL mask = 0ULL;
+        int square_rank = square / 8;
+        int square_file = square % 8;
+
+        // north-east direction
+        for (int r = square_rank + 1, f = square_file + 1; r < 7 && f < 7; r++, f++)
+        {
+            mask |= (1ULL << (f + r * 8));
+        }
+        // north-west direction
+        for (int r = square_rank + 1, f = square_file - 1; r < 7 && f > 0; r++, f--)
+        {
+            mask |= (1ULL << (f + r * 8));
+        }
+        // south-east direction
+        for (int r = square_rank - 1, f = square_file + 1; r > 0 && f < 7; r--, f++)
+        {
+            mask |= (1ULL << (f + r * 8));
+        }
+        // south-west direction
+        for (int r = square_rank - 1, f = square_file - 1; r > 0 && f > 0; r--, f--)
+        {
+            mask |= (1ULL << (f + r * 8));
+        }
+
+        bishop_blocker_masks[square] = mask;
+    }
+}
 
 /**
  * @brief Determines the possible moves for a rook given a blocker configuration.
@@ -206,7 +316,7 @@ void generate_rook_blocker_masks(void)
  */
 ULL determine_possible_rook_moves(uint8_t sq, ULL blocker)
 {
-    ULL result = 0ULL;
+    ULL mask = 0ULL;
     uint8_t square_rank = sq/8; 
     uint8_t square_file = sq%8;
     int rank, file;
@@ -215,28 +325,68 @@ ULL determine_possible_rook_moves(uint8_t sq, ULL blocker)
     // north direction
     for (rank = square_rank+1; rank <= 7; rank++) {
     possible_square = square_file + rank*8;
-    result |= (1ULL << possible_square);
+    mask |= (1ULL << possible_square);
     if (blocker & (1ULL << possible_square)) break;
     }
     // south direction
     for (rank = square_rank-1; rank >= 0; rank--) {
     possible_square = square_file + rank*8;
-    result |= (1ULL << possible_square);
+    mask |= (1ULL << possible_square);
     if (blocker & (1ULL << possible_square)) break;
     }
     // east direction
     for (file = square_file+1; file <= 7; file++) {
     possible_square = file + square_rank*8;
-    result |= (1ULL << possible_square);
+    mask |= (1ULL << possible_square);
     if (blocker & (1ULL << possible_square)) break;
     }
     // west direction
     for (file = square_file-1; file >= 0; file--) {
     possible_square = file + square_rank*8;
-    result |= (1ULL << possible_square);
+    mask |= (1ULL << possible_square);
     if (blocker & (1ULL << possible_square)) break;
     }
-    return result;
+    return mask;
+}
+
+ULL determine_possible_bishop_moves(uint8_t sq, ULL blocker)
+{
+    ULL mask = 0ULL;
+    uint8_t square_rank = sq / 8;
+    uint8_t square_file = sq % 8;
+    int rank, file;
+    uint8_t possible_square;
+
+    // north-east direction
+    for (rank = square_rank + 1, file = square_file + 1; rank <= 7 && file <= 7; rank++, file++)
+    {
+        possible_square = file + rank * 8;
+        mask |= (1ULL << possible_square);
+        if (blocker & (1ULL << possible_square)) break;
+    }
+    // north-west direction
+    for (rank = square_rank + 1, file = square_file - 1; rank <= 7 && file >= 0; rank++, file--)
+    {
+        possible_square = file + rank * 8;
+        mask |= (1ULL << possible_square);
+        if (blocker & (1ULL << possible_square)) break;
+    }
+    // south-east direction
+    for (rank = square_rank - 1, file = square_file + 1; rank >= 0 && file <= 7; rank--, file++)
+    {
+        possible_square = file + rank * 8;
+        mask |= (1ULL << possible_square);
+        if (blocker & (1ULL << possible_square)) break;
+    }
+    // south-west direction
+    for (rank = square_rank - 1, file = square_file - 1; rank >= 0 && file >= 0; rank--, file--)
+    {
+        possible_square = file + rank * 8;
+        mask |= (1ULL << possible_square);
+        if (blocker & (1ULL << possible_square)) break;
+    }
+
+    return mask;
 }
 
 
@@ -264,23 +414,43 @@ ULL determine_possible_rook_moves(uint8_t sq, ULL blocker)
  * Once the function returns true, the magic number is stored in the magic number array,
  * and a new square is passed into the function.
  * 
+ * 
+ * The function has three parts:
+ * - selection of appropriate tables depending on the piece type
+ * - generation of appropriate blocker permutation
+ * - testing and assignment of magic number
+ * 
  * @param square The square index of the piece.
  * @return true if the magic number was generated successfully, false otherwise.
  */
-bool generate_possible_rook_blockers_and_magic_numbers(uint8_t square)
+bool generate_possible_blockers_and_magic_numbers(uint8_t square, bool rook)
 {
-    ULL mask = rook_blocker_masks[square];
+    ULL* magic_numbers_array;
+    ULL* blocker_masks;
+    if (rook)
+    {
+        magic_numbers_array = actual_rook_magic_numbers;
+        blocker_masks = rook_blocker_masks;
+    }
+    else
+    {
+        magic_numbers_array = actual_bishop_magic_numbers;
+        blocker_masks = bishop_blocker_masks;
+    }
+    
+    ULL mask = blocker_masks[square];
     int bits = __builtin_popcountll(mask);
     int permutations = 1 << bits;
     ULL magic_number;
 
     if (KNOWN_MAGIC_NUMBERS) 
     {
-        magic_number = actual_rook_magic_numbers[square];
+        magic_number = magic_numbers_array[square];
     } else 
     {
         magic_number = random_ULL_fewbits();
     }
+
 
     // all the permutations are simply numbers from 0 to 2^bits
     // this code sets the blocker mask bits to that permutation value
@@ -303,33 +473,64 @@ bool generate_possible_rook_blockers_and_magic_numbers(uint8_t square)
             }
         }
 
-        // determine possible moves for that permutation
-        ULL possible_moves = determine_possible_rook_moves(square, blocker);
-        ULL index = (blocker * magic_number) >> (64 - bits);
-        ULL current = rook_attack_lookup_table[square][index];
+
+        if (rook)
+        {
+             // determine possible moves for that permutation
+            ULL possible_moves = determine_possible_rook_moves(square, blocker);
+            ULL index = (blocker * magic_number) >> (64 - bits);
+            ULL current = rook_attack_lookup_table[square][index];
 
 
-        if (KNOWN_MAGIC_NUMBERS) 
-        {
-            rook_attack_lookup_table[square][index] = possible_moves;
-            array_for_rook_magic_numbers[square] = magic_number;
-        } else 
-        {
-            // if there is no collision, store the possible moves in the lookup table
-            if (current == possible_moves) {
-                continue;
-            } else if (!current) {
+            if (KNOWN_MAGIC_NUMBERS) 
+            {
                 rook_attack_lookup_table[square][index] = possible_moves;
-            } else {
-                // counter++;
-                return 0;
+                array_for_rook_magic_numbers[square] = magic_number;
+            } else 
+            {
+                // if there is no collision, store the possible moves in the lookup table
+                if (current == possible_moves) {
+                    continue;
+                } else if (!current) {
+                    rook_attack_lookup_table[square][index] = possible_moves;
+                } else {
+                    // counter++;
+                    return 0;
+                }
+            }
+        } else {
+
+            ULL possible_moves = determine_possible_bishop_moves(square, blocker);
+            ULL index = (blocker * magic_number) >> (64 - bits);
+            ULL current = bishop_attack_lookup_table[square][index];
+
+            if (KNOWN_MAGIC_NUMBERS) 
+            {
+                bishop_attack_lookup_table[square][index] = possible_moves;
+                array_for_bishop_random_numbers[square] = magic_number;
+            } else 
+            {
+                if (current == possible_moves) {
+                    continue;
+                } else if (!current) {
+                    bishop_attack_lookup_table[square][index] = possible_moves;
+                } else {
+                    // counter++;
+                    return 0;
+                }
             }
         }
     }
 
+
+
     if (!KNOWN_MAGIC_NUMBERS) {
         // printf("Collision detected %llu times\n", counter);
-        array_for_rook_magic_numbers[square] = magic_number;
+        if (rook) {
+            array_for_rook_magic_numbers[square] = magic_number;
+        } else {
+            array_for_bishop_random_numbers[square] = magic_number;
+        }
     }
     return 1;
 }
@@ -339,17 +540,28 @@ bool generate_possible_rook_blockers_and_magic_numbers(uint8_t square)
 
 int main(void)
 {
-    generate_rook_blocker_masks();
+    generate_bishop_blocker_masks();
+    for (int i =0; i<64; i++)
+    {
+        print_bitboard(bishop_blocker_masks[i]);
+    }
     for (int j = 0; j < 64; j++)
     {
         printf("Square %d\n", j);
-        while (!generate_possible_rook_blockers_and_magic_numbers(j))
+        while (!generate_possible_blockers_and_magic_numbers(j, false))
         {
             for (int i = 0; i < 4096; i++)
             {
-                rook_attack_lookup_table[j][i] = 0;
+                bishop_attack_lookup_table[j][i] = 0;
             }
         }
+    }
+
+    printf("Bishop magic numbers: \n");
+
+    for (int i = 0; i < 64; i++)
+    {
+        printf("%lluULL,\n", array_for_bishop_random_numbers[i]);
     }
 
 
@@ -358,14 +570,15 @@ int main(void)
         printf("Square: %d\n", i);
         ULL random_position = random_ULL();
         print_bitboard(random_position);
-        ULL blockers = rook_blocker_masks[i] & random_position;
+        ULL blockers = bishop_blocker_masks[i] & random_position;
         print_bitboard(blockers);
 
-        ULL index = (blockers * array_for_rook_magic_numbers[i]) >> offset_RBits[i];
+        ULL index = (blockers * array_for_bishop_random_numbers[i]) >> offset_BBits[i];
         printf("Moves: \n");
-        print_bitboard(rook_attack_lookup_table[i][index]);
+        print_bitboard(bishop_attack_lookup_table[i][index]);
         srand(index);
         printf("\n\n");
 
     }
+
 }
